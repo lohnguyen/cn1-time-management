04338311554de9cb3c98e3d7a4d41348bfa28a21
Troi-Ryan S <troiryans@gmail.com>
2021-02-17 15:02:59 -0800

    add some time span functions

--- src/org/ecs160/a2/Task.java
+++ src/org/ecs160/a2/Task.java
@@ -1,5 +1,6 @@
 package org.ecs160.a2;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,16 +11,22 @@ public class Task {
    private List<TimeSpan> timeSpanList;
    private List<String> tags;
 
-   public Task (String title) {
+   // allow for the construction of a Task based on a title and description
+   public Task (String title, String description) {
       // basic task internals
       this.title = title;
-      this.description = "";
+      this.description = description;
       this.size = "";
 
       // task time internals
       this.totalTime = 0L;
       this.timeSpanList = new ArrayList<TimeSpan>();
       this.tags = new ArrayList<String>();
+   }
+
+   // allow for the construction of a Task based on a title
+   public Task (String title) {
+      this(title, "");
    }
 
    public Task () {
@@ -37,4 +44,22 @@ public class Task {
 
    // for the time internals
    public long getTotalTime () { return this.totalTime; }
+   // NOTE: may remove getTimeSpans in favor of wrapper functions for the list
+   public List<TimeSpan> getTimeSpans () { return this.timeSpanList; }
+   public List<String> getTags() { return this.tags; }
+
+   // get the most recently added time span/window
+   public TimeSpan getMostRecentTimeSpan () {
+      return this.timeSpanList.get(timeSpanList.size() - 1);
+   }
+
+   // checks whether the most recent time span is currently in progress
+   public boolean isInProgress () {
+      TimeSpan currentTimeSpan = this.getMostRecentTimeSpan();
+      // TODO return the check if most recent time span is in progress
+   }
+   
+   public void startNewTimeSpan (LocalDateTime startTime) {
+      this.timeSpanList.add(new TimeSpan(startTime));
+   }
 }
