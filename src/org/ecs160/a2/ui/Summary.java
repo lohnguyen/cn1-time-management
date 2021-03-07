b4b07baaee27e4daaa95a5cb19a66ddc6491ae36
Troi-Ryan S <troiryans@gmail.com>
2021-03-07 14:59:24 -0800

    add tags page

--- src/org/ecs160/a2/ui/Summary.java
+++ src/org/ecs160/a2/ui/Summary.java
@@ -22,7 +22,14 @@ public class Summary extends UpdateableContainer implements AppConstants {
     // the current state of TaskList for this page
     private static List<Task> taskList;
 
-    private UpdateableContainer page1, page2;
+    private UpdateableContainer page1, page2, page3;
+
+    // method to construct page buttons
+    private void addPageButton (Container buttonContainer, String text) {
+        Button pageButton = new Button (text);
+        pageButton.addActionListener((e) -> selectPageButtonAction(e));
+        buttonContainer.add(pageButton);
+    }
 
     /**
      * Default constructor that assembles the children of this container
@@ -36,13 +43,10 @@ public class Summary extends UpdateableContainer implements AppConstants {
                                      FONT_SIZE_TITLE));
 
         // Selection
-        Container buttonContainer = new Container(new GridLayout(1, 2));
-        Button page1Button = new Button ("Everything");
-        page1Button.addActionListener((e) -> selectPageButtonAction(e));
-        buttonContainer.add(page1Button);
-        Button page2Button = new Button ("By Size");
-        page2Button.addActionListener((e) -> selectPageButtonAction(e));
-        buttonContainer.add(page2Button);
+        Container buttonContainer = new Container(new GridLayout(1, 3));
+        addPageButton(buttonContainer, "Everything");
+        addPageButton(buttonContainer, "By Size");
+        addPageButton(buttonContainer, "By Tags");
         this.add(buttonContainer);
 
         // setup the different summary pages
@@ -52,6 +56,9 @@ public class Summary extends UpdateableContainer implements AppConstants {
         this.page2 = new SummarySize();
         this.page2.setHidden(true); // default hidden
         this.add(this.page2);
+        this.page3 = new SummaryTags();
+        this.page3.setHidden(true); // default hidden
+        this.add(this.page3);
 
         // Setup pull to refresh for this container
         this.addPullToRefresh(() -> updateSubContainers());
@@ -64,12 +71,18 @@ public class Summary extends UpdateableContainer implements AppConstants {
         Button button = (Button) e.getComponent();
         switch (button.getText()) {
             case "Everything":
+                this.page3.setHidden(true);
                 this.page2.setHidden(true);
                 this.page1.setHidden(false);
                 break;
             case "By Size":
-                this.page1.setHidden(true);
+                this.page3.setHidden(true);
                 this.page2.setHidden(false);
+                this.page1.setHidden(true);
+            case "By Tags":
+                this.page3.setHidden(false);
+                this.page2.setHidden(true);
+                this.page1.setHidden(true);
         }
     }
 
---
+++ src/org/ecs160/a2/ui/SummaryTags.java
@@ -0,0 +1,76 @@
+package org.ecs160.a2.ui;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.codename1.ui.Display;
+import com.codename1.ui.layouts.BoxLayout;
+import com.codename1.ui.spinner.Picker;
+
+import org.ecs160.a2.models.Task;
+import org.ecs160.a2.ui.containers.UpdateableContainer;
+import org.ecs160.a2.ui.containers.TaskContainer;
+import org.ecs160.a2.ui.containers.StatsContainer;
+import org.ecs160.a2.utils.AppConstants;
+import org.ecs160.a2.utils.UIUtils;
+
+/**
+ * The container that houses the Summary containers for different sizes
+ */
+public class SummaryTags extends UpdateableContainer implements AppConstants {
+
+    // label containers
+    private UpdateableContainer tasks, stats;
+
+    // picker that allows the selection of sizes
+    private Picker sizePicker;
+
+    /**
+     * Assemble the children of this container
+     */
+    public SummaryTags () {
+        super(new BoxLayout(BoxLayout.Y_AXIS));
+
+        // size picker that updates everything on state change
+        String[] sizes = Task.sizes.toArray(new String[Task.sizes.size()]);
+        this.sizePicker = new Picker();
+        this.sizePicker.setType(Display.PICKER_TYPE_STRINGS);
+        this.sizePicker.setStrings(sizes);
+        this.sizePicker.setSelectedStringIndex(1); // default to "S"
+        this.sizePicker.addActionListener((e) -> askParentForUpdate());
+        this.add(this.sizePicker);
+
+        // Tasks
+        this.add(UIUtils.createLabel("Tasks", NATIVE_BOLD, COLOR_TITLE,
+                                     FONT_SIZE_SUB_TITLE));
+        this.tasks = new TaskContainer();
+        this.add(this.tasks);
+
+        // Stats
+        this.add(UIUtils.createLabel("Statistics", NATIVE_BOLD, COLOR_TITLE,
+                                     FONT_SIZE_SUB_TITLE));
+        this.stats = new StatsContainer();
+        this.add(this.stats);
+    }
+
+    // filter a task list for the specified size
+    private List<Task> filterTaskList(List<Task> taskList, String size) {
+        List<Task> returnList = new ArrayList<Task>();
+        for (Task task : taskList) {
+            if (task.getSize().equals(size)) returnList.add(task);
+        }
+        return returnList;
+    }
+
+    /**
+     * Update the sub containers after filtering the Task List for the current
+     * size
+     */
+    @Override
+    public void updateContainer(List<Task> taskList) {   
+        List<Task> filteredList;
+        filteredList = filterTaskList(taskList, sizePicker.getSelectedString());
+        this.tasks.updateContainer(filteredList);
+        this.stats.updateContainer(filteredList);
+    }
+}
